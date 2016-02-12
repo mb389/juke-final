7ba3bd62e207ac577a601004172794b78fa478d9
Jai Kamat <jaipkamat@gmail.com>
2016-02-12 10:12:29 -0500

    premerge conflict

--- browser/js/playlist/playlist.controllers.js
+++ browser/js/playlist/playlist.controllers.js
@@ -1,10 +1,8 @@
 juke.controller('PlaylistCtrl', function ($scope, $rootScope, PlayerFactory, PlaylistFactory) {
 
-
-
    $scope.addTitle = function() {
       PlaylistFactory.create($scope.playlistInput);
-      $scope.playlistInput.name="";
+      // $scope.playlistInput.name="";
       $scope.playlistForm.name.$touched=false;
       console.log($scope.playlistInput);
    }
